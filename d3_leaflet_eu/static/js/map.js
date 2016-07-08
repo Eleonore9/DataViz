175ac457ad977159c99d3c8a6780ece184b04740
Eleonore9 <eleonore.mayola@ymail.com>
2016-07-08 17:32:36 +0100

    Managed to get the Leaflet map display

--- d3_leaflet_eu/static/js/map.js
+++ d3_leaflet_eu/static/js/map.js
@@ -1,1 +1,8 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+$(document).ready(function() {
+  var myMap = L.map('map').setView([37.8, -96.9], 4);
+
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+});
