0a9708df282bea37e2b2541850eb4c83deb15d5f
Eleonore9 <eleonore.mayola@ymail.com>
2016-07-31 20:05:25 +0100

    Add geojson

--- d3_leaflet_eu/static/js/map.js
+++ d3_leaflet_eu/static/js/map.js
@@ -2,6 +2,14 @@
 $(document).ready(function() {
   var myMap = L.map('map').setView([37.8, -96.9], 4);
 
+  var svg = d3.select(myMap.getPanes().overlayPane).append("svg"),
+    g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+  d3.json("us-states.json", function(error, collection) {
+    if (error) throw error;
+
+  });
+
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(myMap);
