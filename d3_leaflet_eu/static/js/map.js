12323b4093a507e1b52de2ffe6aa8d26a034e083
Eleonore Mayola <eleonore.mayola@ymail.com>
2016-08-19 17:25:23 +0100

    Reset svg position + styling

--- d3_leaflet_eu/static/js/map.js
+++ d3_leaflet_eu/static/js/map.js
@@ -13,25 +13,31 @@ $(document).ready(function() {
     if (error) throw error;
     
     var transform = d3.geoTransform({point: projectPoint}),
-    path = d3.geoPath().projection(transform);
+      path = d3.geoPath().projection(transform);
     
     var feature = g.selectAll("path")
-    .data(collection.features)
-    .enter().append("path");
+        .data(collection.features)
+        .enter().append("path");
 
-    feature.attr("d", path);
+    myMap.on("viewreset", reset);
+    reset();
 
-    var bounds = path.bounds(collection),
-    topLeft = bounds[0],
-    bottomRight = bounds[1];
+    // Reposition the SVG to cover the features.
+    function reset() {
+      var bounds = path.bounds(collection),
+        topLeft = bounds[0],
+        bottomRight = bounds[1];
 
-    svg .attr("width", bottomRight[0] - topLeft[0])
+    svg.attr("width", bottomRight[0] - topLeft[0])
     .attr("height", bottomRight[1] - topLeft[1])
     .style("left", topLeft[0] + "px")
     .style("top", topLeft[1] + "px");
 
-    g   .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-  });
+    g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+  
+    feature.attr("d", path);
+  }
+});
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
