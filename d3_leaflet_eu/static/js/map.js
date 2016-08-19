d3a787e8769e23078db0009e58f23d58122951eb
Eleonore Mayola <eleonore.mayola@ymail.com>
2016-08-19 16:36:32 +0100

    WIP: Project geojson, new geojson file

--- d3_leaflet_eu/static/js/map.js
+++ d3_leaflet_eu/static/js/map.js
@@ -1,13 +1,36 @@
-// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 $(document).ready(function() {
   var myMap = L.map('map').setView([37.8, -96.9], 4);
 
   var svg = d3.select(myMap.getPanes().overlayPane).append("svg"),
     g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
-  d3.json("us-states.json", function(error, collection) {
-    if (error) throw error;
+  function projectPoint(x, y) {
+    var point = myMap.latLngToLayerPoint(new L.LatLng(y, x));
+    this.stream.point(point.x, point.y);
+  }
 
+  d3.json("static/js/us-states.json", function(error, collection) {
+    if (error) throw error;
+    
+    var transform = d3.geoTransform({point: projectPoint}),
+    path = d3.geoPath().projection(transform);
+    
+    var feature = g.selectAll("path")
+    .data(collection.features)
+    .enter().append("path");
+
+    feature.attr("d", path);
+
+    var bounds = path.bounds(collection),
+    topLeft = bounds[0],
+    bottomRight = bounds[1];
+
+    svg .attr("width", bottomRight[0] - topLeft[0])
+    .attr("height", bottomRight[1] - topLeft[1])
+    .style("left", topLeft[0] + "px")
+    .style("top", topLeft[1] + "px");
+
+    g   .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
   });
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
